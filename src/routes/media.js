8a2ce69a436a1df20f113177bd8819ec6a33149c
root <keithohno@gmail.com>
2022-04-28 00:28:54 +0000

    connected to seraphine and removed mongo dependency

--- src/routes/media.js
+++ src/routes/media.js
@@ -1,65 +1,46 @@
 import express from "express";
-import mongoose from "mongoose";
-import Media from "../db/media";
+import FormData from "form-data";
+import multer from "multer";
+import fetch from "node-fetch";
 import { isAuthenticated } from "../util/auth";
 
 const router = express.Router();
-const multer = require("multer");
-const fs = require("fs");
 
-const storage = multer.diskStorage({
-  destination: function (req, file, callback) {
-    callback(null, "/home/pantheon/uploads");
-  },
-  filename: function (req, file, callback) {
-    callback(null, Date.now() + "-" + file.originalname);
-  },
-});
+const upload = multer();
 
-const upload = multer({ storage: storage });
-
-// upload, name must be 'media'
+// upload
 router.post(
   "/upload",
   isAuthenticated,
   upload.single("file"),
   async (req, res) => {
-    if (
-      req.file &&
-      (req.file.mimetype == "image/jpeg" || req.file.mimetype == "image/png")
-    ) {
-      // if file received, create mongoDB Media entry, then return that object ID
-      // create new user
-      let media = new Media({
-        filename: req.file.filename,
-        originalname: req.file.originalname,
-        mimetype: req.file.mimetype,
-        path: req.file.path,
-      });
-      console.log(media);
-      await media.save();
-      res.json({ mediaid: media._id });
-    } else {
-      console.log("File unsuccessfully uploaded");
-      console.log(req.file);
-      res.json({
-        error: true,
-        description: "File is of wrong type or does not exist.",
-      });
-    }
+    if (!req.file || !req.file.originalname || !req.file.buffer)
+      return res.json({ error: true, description: "invalid file" });
+
+    const form = new FormData();
+    form.append("file", req.file.buffer, req.file.originalname);
+    let threshres = await (
+      await fetch(`http://${process.env.SERAPHINE_URL}/upload`, {
+        method: "POST",
+        body: form,
+      })
+    ).json();
+
+    res.json(threshres);
   }
 );
 
+// access
 router.get("/access/:id", isAuthenticated, async (req, res) => {
-  // console.log(mongoose.Types.ObjectId(req.params.id))
-  console.log(req.params.id);
-  Media.findById(req.params.id).then(function (r) {
-    console.log(r.path);
-    res.sendFile(r.path);
-  });
+  let threshres = await fetch(
+    `http://${process.env.SERAPHINE_URL}/access/${req.params.id}`
+  );
+  if (threshres.status !== 200) res.json(await threshres.json());
+  else {
+    let blob = await threshres.blob();
+    res.set("Content-Type", blob.type);
+    res.send(Buffer.from(await blob.arrayBuffer()));
+  }
 });
 
-router.use("/test", isAuthenticated, async (req, res) => {
-  res.send("hit /media/test");
-});
 export default router;
