0845a0ea2489a86c0e7e4740b704bf88a8b0532c
root <keithohno@gmail.com>
2022-05-02 05:04:21 +0000

    fixed hash

--- src/upstream.js
+++ src/upstream.js
@@ -5,7 +5,8 @@ const upsplit = upstreams.length;
 export const naut_url = (docid) => {
   let res = 0;
   for (var i = 0; i < docid.length; i++) {
-    res = res * 37 + docid.charCodeAt(i);
+    res = (res << 5) - res + docid.charCodeAt(i);
+    res |= 0;
   }
-  return upstreams[res % upsplit];
+  return upstreams[((res % upsplit) + upsplit) % upsplit];
 };
